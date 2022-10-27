68c9e9e00509d236d67db796962601f63d228c94
truongbao172 <truongbao677@gmail.com>
2022-10-27 22:32:25 +0700

    finish home page

--- src/components/IdeasPost/index.js
+++ src/components/IdeasPost/index.js
@@ -3,16 +3,29 @@ import "./styles.scss"
 function IdeasPost() {
   return (
     <div id='ideaPost'>
-        <div className='page-main mx-auto'>
-                <div className='row '>
-                    <div className='col-6 ideas-post-img order-md-1 order-1'>
-                        <img src="https://www.tikibrand.com/media/ideas/Outdoor_Movie_Night_1-updated.jpg" alt="" />
-                    </div>
-                    <div className='col-6 ideas-post-desc order-md-2 order-2 bg-yellow'>
-asa
-                    </div>
-                </div>
+      <div className='page-main mx-auto'>
+        <div className='row ideas-post'>
+          <div className='col-6 ideas-post-img order-md-1 order-1'>
+            <img src="https://www.tikibrand.com/media/ideas/Outdoor_Movie_Night_1-updated.jpg" alt="" />
+          </div>
+          <div className='col-6 ideas-post-desc order-md-2 order-2 bg-yellow'>
+            <div className="mt-5 mb-3">
+              <a title="OUTDOOR MOVIE NIGHT" href="/ideas" className="ideas-post-title text-decoration-underline color-tiki-black">TIKI IDEAS</a>
+            </div>
+            <div className="ideas-post-number fw-bold">
+              <span>NO.391</span>
+            </div>
+            <h2 className="ideas-post-heading mt-1 mb-3 fw-bold">
+              OUTDOOR MOVIE NIGHT
+            </h2>
+            <p className="tiki-idea-post-text">
+              Summer nights are very enjoyable, so take advantage of the weather by taking family movie night out to the backyard. Bring back a classic, stream your favorite game or turn on the latest hit-- at the end of the day, the memories you make are the greatest gifts these nights give us!
+            </p>
+            <p className="tiki-idea-post-tags text-uppercase pb-5">
+              Topics:<a class="ideas-tags" href="/ideas/?cat=ideas-filters-option-outdoor-living-everyday"><span class="ideas-filters-option-outdoor-living-everyday"> Everyday</span></a><a class="ideas-tags" href="/ideas/?cat=ideas-filters-option-space-backyard"><span class="ideas-filters-option-space-backyard">, Backyard</span></a></p>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
--- src/pages/MyAccount/index.js
+++ src/pages/MyAccount/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import "./styles.scss"
 const MyAccount = () => {
   return (
-    <div>MyAccount</div>
+    <div className='myAccount'>
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item"><a href="/">Home</a></li>
+        <li className="breadcrumb-item active" aria-current="page">Library</li>
+      </ol>
+    </nav>
+
+    </div>
   )
 }
 
